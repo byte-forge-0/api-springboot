83c8345844ab0a986a1e24f41593ef819acf5808
Olavo15 <olavoregis169@gmail.com>
2026-06-07 14:05:29 -0300

    feat: adicionar anotações Lombok à entidade motorpuchase

--- src/main/java/com/example/demo/model/motorpuchase.java
+++ src/main/java/com/example/demo/model/motorpuchase.java
@@ -4,11 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "usuario")
 @Getter
-
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class motorpuchase {
     @Id
     private Long id;
